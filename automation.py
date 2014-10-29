81bffb0efd45ea90a1b31c297abb582edd432925
riskkim <wnsgh611@gmail.com>
2014-10-29 19:59:02 +0900

    add requirement module, argument

--- automation.py
+++ automation.py
@@ -2,10 +2,12 @@ __author__ = 'junojunho / wnsgh611@gmail.com'
 
 # This file is for openstack automation project
 # Total procedure is divided into 2 parts, keystone auth part, and swift access part.
+# Required Module : python-swift, python-keystoneclient
 
 # First big step is Keystone auth part
 # Keystone auth part is for get token from openstack - keystone for swift auth id & password
 # In keystone, we will create a user & tenant, we will set role for swift each user & tenant
+# We will use bypassing method with os_auth_url(auth_url), os_service_token(token), os_service_endpoint(endpoint)
 
 
 # Second big step is Swift auth part
